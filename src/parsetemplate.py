25e07c3660ede43986dcbf9ddd9242212976c95c
Koray Atalag <koray.atalag@gmail.com>
2017-02-12 03:45:15 +1300

    Home: finally getting termBindings in parsetemplate

--- src/parsetemplate.py
+++ src/parsetemplate.py
@@ -2,40 +2,28 @@ import tehr_helpers
 import json
 import pprint
 
-def unpack_annotations():
-    #aql = binds[::2]
-    #term = binds[1::2]
 
-    #print(aql)
-    #print(term)
+def unpack_annotations(term, aql):
 
-    s = []
-    o = []
-    p = []
+    for d in term.values():
+        tId = d['terminologyId']
+        tVal = d['value']
 
-    for i in aql:
-        #print(i)
-        s.append(i)
+        print()
+        print('path = ' + aql)
+        print('terminologyId = ' + tId)
+        print('code = ' + tVal)
 
-    for t in term:
-        #print(t[next(iter(t))]['terminologyId'])
-        p.append(t[next(iter(t))]['terminologyId'])
-        #print(t[next(iter(t))]['value'])
-        o.append(t[next(iter(t))]['value'])
-
-    for l in range(0, len(aql)):
-        print(str(l))
-        print('   path = ' + s[l])
-        print('   terminologyId = ' + p[l])
-        print('   code = ' + o[l])
-
-
-#Alternative parser
-def item_generator(json_input, lookup_key):     #gets all termBindings but not aqlPath :(
+#Generator parser
+def item_generator(json_input, lookup_key):
     if isinstance(json_input, dict):
         for k, v in json_input.items():
             if k == lookup_key:
-                aq = item_generator(json_input, 'aqlPath')
+                #aq = item_generator(json_input, 'aqlPath')
+                try:
+                    aq = json_input['aqlPath']
+                except:
+                    aq = "None"
                 yield v, aq
             else:
                 for child_val in item_generator(v, lookup_key):
@@ -45,46 +33,16 @@ def item_generator(json_input, lookup_key):     #gets all termBindings but not a
             for item_val in item_generator(item, lookup_key):
                 yield item_val
 
-def get_recursively(search_dict, field):
-    fields_found = []
-    aql_path = ""
-    for key, value in search_dict.items():
-        if key == field:
-            fields_found.append(value)
-            if search_dict['aqlPath'] != "":
-                aql_path = search_dict['aqlPath']
-
-        elif isinstance(value, dict):
-            results = get_recursively(value, field)
-            for result in results:
-                fields_found.append(result)
-
-        elif isinstance(value, list):
-            for item in value:
-                if isinstance(item, dict):
-                    more_results = get_recursively(item, field)
-                    for another_result in more_results:
-                        fields_found.append(another_result)
-
-    return fields_found, aql_path
-
 def searchTemplate (t):
-    tbinds, aql = get_recursively(t, 'termBindings')
-    pprint.pprint(tbinds, aql)
-    quit()
 
-    #tbinds = item_generator(t, 'termBindings')
+    tbinds = item_generator(t, 'termBindings')
 
     for t, a in tbinds:
-        x = next(a, 'No AQL')
-        if x != 'No AQL':
-            print(t, x[0])
-    print('End of first pass')
+        #print(t, a)
+        unpack_annotations(t, a)
 
 #wt = tehr_helpers.getWebTemplate(templateName='KorayClinical4')
 with open('..\\models\KorayClinical4-webtemplate.json') as webTemplateFile:
     wt = json.load(webTemplateFile)
 
-
-#aql_path = []
 searchTemplate(wt)
