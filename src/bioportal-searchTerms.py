ba37a69a441f23fcc03e1863147e068adfd0dea9
Koray Atalag <koray.atalag@gmail.com>
2017-03-01 19:36:44 +1300

    Minor changes to bioportal API - will change to generic ontology lookup service

--- src/bioportal-searchTerms.py
+++ src/bioportal-searchTerms.py
@@ -1,12 +1,11 @@
 import bioportal
-import requests
 from pprint import pprint
 
 include = ''
 include = "&ontologies=SNOMEDCT"
 include += "&display_context=false"
 #include += "&cui=C0018799"
-include += "&include=prefLabel,cui"
+include += "&include=prefLabel,cui" # ,cui
 #include += "&suggest=true"
 include += "&require_exact_match=true"
 include += "&display_links=false"
@@ -16,7 +15,8 @@ resources = bioportal.get_json("/")
 
 
 # Get list of search terms
-search_terms = ['Acute coronary syndrome']     #, 'gastritis', 'experiment', 'human', 'brain', 'melanoma']
+#search_terms = ['http%3A%2F%2Fbhi.washington.edu%2FOPB%23OPB_00340', 'http%3A%2F%2Fbhi.washington.edu%2FOPB%23OPB_01023']     #, 'gastritis', 'experiment', 'human', 'brain', 'melanoma']
+search_terms = ['Acute Coronary Syndrome']
 terms = []
 for line in search_terms:
     terms.append(line)
@@ -25,7 +25,7 @@ for line in search_terms:
 search_results = []
 for term in terms:
     search_results.append("You searched: " + term)
-    search_results.append(bioportal.get_json("/search?q=" + term + include)["collection"])
+    search_results.append(bioportal.get_json("/search?q=" + term + include)["collection"])   #[0]["prefLabel"])
 
 # Print the results
 for result in search_results:
